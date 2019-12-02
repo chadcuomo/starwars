a2d6798654a4888d73cc24c1d4edd78bfaf11aa9
Chad <chad.cuomo@yahoo.com>
2019-12-02 15:42:30 -0500

    Added character images as well as added card component styles

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import './card.css';
 
-const Card = ({ name }) => {
+const Card = ({ name, image }) => {
+
+  let imagePath = '';
+  try {
+    require(`../../images/${image}.jpg`);
+    imagePath = image + '.jpg';
+  }
+  catch (err) {
+    try {
+      require(`../../images/${image}.png`)
+      imagePath = image + '.png';
+    }
+    catch (err) {
+      imagePath = 'default.jpg';
+    }
+  }
+ 
+
+  
   return (
-    <d1>
-      <dt>Name</dt>
-      <dd>{name}</dd>
-    </d1>
+    <div className='card'>
+      <img alt='person' src ={require(`../../images/${imagePath}`)} />
+      <h3>{name}</h3>
+    </div>
   );
 }
 
